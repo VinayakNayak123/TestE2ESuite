5fa0e63a3c621f6c37440af69ad17bac5adb6d18
VinayakNayak123 <vinayak123nayak@gmail.com>
2024-12-11 12:17:06 +0530

    TEST-1234 added practiceTestmethod

--- src/test/java/techstats/SaleTest/SaleTransactionTest.java
+++ src/test/java/techstats/SaleTest/SaleTransactionTest.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -97,6 +99,22 @@ public class SaleTransactionTest extends TechstatsBase{
 				
 				String confirmMessage = driver.findElement(By.cssSelector(".hero-primary")).getText();
 				Assert.assertTrue(confirmMessage.equalsIgnoreCase("THANKYOU FOR THE ORDER."));
+	}
+	
+	
+	@Test
+	public void practiceTest()
+	{
+		//work in progress
+		String browserName = prop.getProperty("browser");
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/SeleniumWebdriver/chromedriver.exe");
+		driver= new ChromeDriver(options);
+		
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get(prop.getProperty("url"));
 	}
 	
 	
